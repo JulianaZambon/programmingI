e03e8cd6db5cf5e37f5c56875cbcac4bcec63d38
Juliana Zambon <julianazambon@ufpr.br>
2023-06-01 16:35:04 -0300

    [ADD]atualizacao

---
+++ prep2/liblista_circular.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include "liblista_circular.h"
+
+/*
+ * elemento_t possui o campo chave, quem sabe pode ter mais coisas depois
+*/
+typedef struct elemento {
+    int chave;  /* chave da struct */
+    /* pode ter mais campos, mas a chave sera usada sempre nas operacoes */
+} elemento_t;
+
+/*
+ * nodo_t eh uma estrutura interna que representa cada nodo da Lista.
+ * Cada nodo aponta para o proximo e tambem para o anterior.
+ * Externamente o usuario da biblioteca nao vai poder usa-la, pois
+ * nao pode ter acesso direto ao ponteiro prox.
+*/
+typedef struct nodo {
+    elemento_t *elemento;
+    struct nodo *prox;
+    struct nodo *prev;
+} nodo_t;
+
+/*
+ * Representacao da Lista 
+*/
+typedef struct lista {
+    nodo_t *ini;
+} lista_t;
+
+/* 
+ * Cria uma Lista vazia 
+*/
+lista_t *lista_cria ()
+{
+
+}
+
+/* 
+ * Destroi a Lista e a aponta para NULL
+*/ 
+void lista_destroi (lista_t **l)
+{
+
+}
+
+/* 
+ * Adiciona um elemento em ordem de acordo com o valor elemento->chave na Lista. 
+ * Retorna 1 em caso de sucesso e 0 caso contrario.
+*/
+int lista_insere_ordenado (lista_t *l, elemento_t *elemento)
+{
+
+}
+
+/* 
+ * Retira o elemento da Lista e a mantem em ordem.
+ * Retorna 1 em caso de sucesso e 0 caso elemento nao esteja na Lista.
+*/
+int lista_remove_ordenado (lista_t *l, elemento_t *elemento)
+{
+
+}
+
+/* Retorna 1 se a lista esta vazia e 0 caso contrario */
+int lista_vazia (lista_t *l)
+{
+    
+}
---
+++ prep2/testa_lista_circular.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+#include "liblista_circular.h"
